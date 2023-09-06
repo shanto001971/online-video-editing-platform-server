953c2c544a9c07ef19c08ab9e11e6d6e89a786cb
TobiborRahman <r.tobibor16@gmail.com>
2023-09-06 12:50:11 +0600

    make admin patch request added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -88,6 +88,18 @@ async function run() {
 
 		app.get('/users', async (req, res) => {
 			const result = await usersCollection.find().toArray();
+			res.send(result);
+		});
+
+		app.patch('/users/admin/:id', async (req, res) => {
+			const id = req.params.id;
+			const filter = { _id: new ObjectId(id) };
+			const updateDoc = {
+				$set: {
+					role: 'admin',
+				},
+			};
+			const result = await usersCollection.updateOne(filter, updateDoc);
 			res.send(result);
 		});
 
