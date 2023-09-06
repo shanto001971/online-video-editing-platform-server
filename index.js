ec0d3b2b1396e2166e1705415b9953f928c08b34
TobiborRahman <r.tobibor16@gmail.com>
2023-09-06 13:30:03 +0600

    user deleted api added

--- index.js
+++ index.js
@@ -113,6 +113,14 @@ async function run() {
 			res.send(result);
 		});
 
+		// user delete api
+		app.delete('/users/:id', async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: new ObjectId(id) };
+			const result = await usersCollection.deleteOne(query);
+			res.send(result);
+		});
+
 		// Send a ping to confirm a successful connection
 		await client.db('admin').command({ ping: 1 });
 		console.log(
