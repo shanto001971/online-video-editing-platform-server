3a272b67e9fa25cdf2e761b790254328ee900d5c
Emon <emonhasan7650@gmail.com>
2023-09-11 19:24:07 +0600

    upadte user by email functionality added

--- index.js
+++ index.js
@@ -143,6 +143,19 @@ async function run() {
 			res.send(result);
 		})
 
+	//Update user by email in DB
+    app.put("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const query = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await usersCollection.updateOne(query, updateDoc, options);
+      res.send(result);
+    });
+
 		// users api ended here 
 
 		// Send a ping to confirm a successful connection
