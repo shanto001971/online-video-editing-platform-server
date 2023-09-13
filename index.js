0365c2dd03153e0bb53ef161904488697cb73568
TobiborRahman <r.tobibor16@gmail.com>
2023-09-13 23:28:01 +0600

    pulled and push

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-// const SSLCommerzPayment = require('sslcommerz-lts');
+
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
