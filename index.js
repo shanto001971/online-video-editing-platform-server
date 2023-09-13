aa19d2086466db628b353fb95da96d209ed6b9d4
TobiborRahman <r.tobibor16@gmail.com>
2023-09-13 22:49:32 +0600

    demo collections has been created

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const SSLCommerzPayment = require('sslcommerz-lts');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
@@ -93,7 +94,7 @@ async function run() {
 			next();
 		};
 
-		//TODO: Warning data insert  on the mongodb
+		// Warning data insert  on the mongodb
 		app.get('/demoVideoTemplate', async (req, res) => {
 			const result = await demoVideoTemplate.find().toArray();
 			res.send(result);
@@ -108,9 +109,53 @@ async function run() {
 		});
 
 		// Payment methods api is here
-		app.post('/payments', async (req, res) => {
-			console.log(req.body);
-		});
+		// const tran_id = new ObjectId().toString();
+		// app.post('/payments', async (req, res) => {
+		// 	const value = req.body;
+		// 	const price = value.price;
+		// 	const data = {
+		// 		total_amount: price,
+		// 		currency: 'BDT',
+		// 		tran_id: tran_id, // use unique tran_id for each api call
+		// 		success_url: 'http://localhost:3030/success',
+		// 		fail_url: 'http://localhost:3030/fail',
+		// 		cancel_url: 'http://localhost:3030/cancel',
+		// 		ipn_url: 'http://localhost:3030/ipn',
+		// 		shipping_method: 'Courier',
+		// 		product_name: 'Computer.',
+		// 		product_category: 'Electronic',
+		// 		product_profile: 'general',
+		// 		cus_name: 'Customer Name',
+		// 		cus_email: 'customer@example.com',
+		// 		cus_add1: 'Dhaka',
+		// 		cus_add2: 'Dhaka',
+		// 		cus_city: 'Dhaka',
+		// 		cus_state: 'Dhaka',
+		// 		cus_postcode: '1000',
+		// 		cus_country: 'Bangladesh',
+		// 		cus_phone: '01711111111',
+		// 		cus_fax: '01711111111',
+		// 		ship_name: 'Customer Name',
+		// 		ship_add1: 'Dhaka',
+		// 		ship_add2: 'Dhaka',
+		// 		ship_city: 'Dhaka',
+		// 		ship_state: 'Dhaka',
+		// 		ship_postcode: 1000,
+		// 		ship_country: 'Bangladesh',
+		// 	};
+		// 	console.log(data);
+		// 	const sslcz = new SSLCommerzPayment(
+		// 		store_id,
+		// 		store_passwd,
+		// 		is_live
+		// 	);
+		// 	sslcz.init(data).then((apiResponse) => {
+		// 		// Redirect the user to payment gateway
+		// 		let GatewayPageURL = apiResponse.GatewayPageURL;
+		// 		res.send({ url: GatewayPageURL });
+		// 		console.log('Redirecting to: ', GatewayPageURL);
+		// 	});
+		// });
 
 		// users api started here
 		app.post('/users', async (req, res) => {
