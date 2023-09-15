bffe5c4fed0ae6fac20455fee8f8f5ec2fcf4dac
chayansrk64 <chayansrk64@gmail.com>
2023-09-15 19:46:11 +0600

    user delete api updated

--- index.js
+++ index.js
@@ -145,6 +145,7 @@ async function run() {
 			res.send(result);
 		})
 
+		// user delete api
 		app.delete('/users/:id', async(req, res) => {
 		const id = req.params.id;
 		const query = {_id: new ObjectId(id)}
